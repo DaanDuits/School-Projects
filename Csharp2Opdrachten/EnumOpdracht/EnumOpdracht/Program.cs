3c210f051aba1416f00b58686cabc032cbb7a516
DaanDuits <daand04@gmail.com>
2022-05-10 08:53:23 +0200

    list en enums opdrachten gemaakt

---
+++ Csharp2Opdrachten/EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EnumOpdracht
+{
+    class Program
+    {
+        internal enum Colors
+        {
+            Red,
+            Green,
+            Yellow,
+            Blue
+        }
+        static void Main(string[] args)
+        {
+            Colors[] colors = Enum.GetValues(typeof (Colors)) as Colors[];
+            foreach(Colors color in colors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadLine();
+            string value = Console.ReadLine();
+
+            Colors parsedColor = (Colors)Enum.Parse(typeof (Colors), value);
+        }
+    }
+}
---
+++ Csharp2Opdrachten/ListOpdracht/ListOpdracht/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> birds = new List<string>();
+            birds.Add("uil");
+            birds.Add("kraai");
+            birds.Add("hond");
+            birds.Add("papegaai");
+
+            foreach(string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+
+            List<string> birds2 = new List<string>();
+            birds2.Add("meeuw");
+            birds2.Add("duif");
+            
+            Console.WriteLine(" ");
+            foreach (string bird in birds2)
+            {
+                Console.WriteLine(bird);
+            }
+
+            birds.AddRange(birds2);
+
+            Console.WriteLine(" ");
+            foreach (string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+
+            birds.RemoveAt(birds.IndexOf("hond"));
+            if(birds.Contains("kip"))
+            {
+                birds.RemoveAt(birds.IndexOf("kip"));
+            }
+
+            Console.WriteLine(" ");
+            foreach (string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+            Console.ReadLine();
+        }
+    }
+}
